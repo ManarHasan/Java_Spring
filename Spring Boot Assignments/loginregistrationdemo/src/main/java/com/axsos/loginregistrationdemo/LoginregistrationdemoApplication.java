29b38b073be4d383147a710e947b70dfca6b9a68
manarHasan <manar.hasan62@gmail.com>
2021-01-14 11:38:39 +0200

    Login and Registration Step 1

---
+++ Spring Boot Assignments/loginregistrationdemo/src/main/java/com/axsos/loginregistrationdemo/LoginregistrationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.axsos.loginregistrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginregistrationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginregistrationdemoApplication.class, args);
+	}
+
+}
